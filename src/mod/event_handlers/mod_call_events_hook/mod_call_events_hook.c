8f30f8ea751ad35c2cf8fd90c8bfcce75d62fcc9
Osama Shafqat <osamashafqat@msn.com>
2025-05-24 03:15:38 +0500

    Code modify for recording and other events

--- src/mod/event_handlers/mod_call_events_hook/mod_call_events_hook.c
+++ src/mod/event_handlers/mod_call_events_hook/mod_call_events_hook.c
@@ -26,8 +26,10 @@ static void free_call_event_info(call_event_info_t *info) {
         switch_safe_free(info->call_direction);
         switch_safe_free(info->caller_number);
 		switch_safe_free(info->call_uuid);
-        switch_safe_free(info->recording_file_path);
-  switch_safe_free(info->caller_name);      
+        if(info->recording_file_path != NULL){
+            switch_safe_free(info->recording_file_path);
+        } 
+        switch_safe_free(info->caller_name);      
 		switch_safe_free(info);
     }
 }
@@ -40,18 +42,19 @@ char *convert_to_json(call_event_info_t *obj) {
   
       
 		cJSON_AddStringToObject(root, "name", obj->event);
-                cJSON_AddStringToObject(data, "domain", obj->domain ? obj->domain : "");
+        cJSON_AddStringToObject(data, "domain", obj->domain ? obj->domain : "");
         cJSON_AddStringToObject(data, "event", obj->event ? obj->event : "");
         cJSON_AddStringToObject(data, "destination_number", obj->destination_number ? obj->destination_number : "");
         cJSON_AddStringToObject(data, "direction", obj->call_direction ? obj->call_direction : "");
         cJSON_AddStringToObject(data, "caller_number", obj->caller_number);
 		cJSON_AddStringToObject(data, "call_uuid", obj->call_uuid );
-  cJSON_AddStringToObject(data,"caller_name", obj->caller_name);
-        cJSON_AddStringToObject(data,"recording_file_path", obj->recording_file_path);
-		cJSON_AddItemToObject(root,"data",data);
+        cJSON_AddStringToObject(data,"caller_name", obj->caller_name);
+        cJSON_AddStringToObject(data, "recording_file_path", obj->recording_file_path ? obj->recording_file_path : "");
+        cJSON_AddItemToObject(root,"data",data);
 		json_string = cJSON_Print(root);
 		cJSON_Delete(root);
     }
+    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "JSON String: %s\n", json_string);
     return json_string;
 }
 
@@ -83,7 +86,7 @@ static void send_event_data(call_event_info_t *data) {
 
 static void event_handler(switch_event_t *event) {
     const char *event_name = switch_event_get_header(event, "Event-Name");
-
+    call_event_info_t *call_info = NULL;
     if (event_name) {
         const char *domain_name = switch_event_get_header(event, "variable_domain_name");
         const char *call_direction = switch_event_get_header(event, "Caller-Direction");
@@ -101,11 +104,32 @@ static void event_handler(switch_event_t *event) {
             strcmp(event_name, "CHANNEL_CREATE") == 0 ||
             strcmp(event_name, "CHANNEL_PROGRESS") == 0||
             strcmp(event_name, "RECORD_STOP") == 0||
-            strcmp(event_name, "CHANNEL_PROGRESS_MEDIA") == 0){ 
+            strcmp(event_name, "CHANNEL_PROGRESS_MEDIA") == 0 ||
+            strcmp(event_name, "CHANNEL_HANGUP_COMPLETE") == 0 ){ 
             
-            call_event_info_t *call_info = (call_event_info_t*) malloc(sizeof(call_event_info_t));
+                if(strcmp(event_name, "RECORD_STOP")==0 && switch_file_exists(recording_file, NULL) == SWITCH_STATUS_SUCCESS){
+                    call_event_info_t *record_info = (call_event_info_t*) malloc(sizeof(call_event_info_t)); 
+                    if(recording_file && record_info){
+
+                        record_info->call_uuid = switch_safe_strdup(call_uuid);
+                        record_info->event = switch_safe_strdup(event_name);
+                        record_info->domain = switch_safe_strdup(domain_name);
+                        record_info->destination_number = switch_safe_strdup(callee_number);
+                        record_info->caller_number = switch_safe_strdup(caller_number);
+                        record_info->call_direction = switch_safe_strdup(call_direction); 
+                        record_info->recording_file_path = switch_safe_strdup(recording_file);
+                        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Recording file path: %s\n", recording_file);
+                        
+                        send_event_data(record_info);
+                        free_call_event_info(record_info);
+                    }
+                    else{
+                        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to allocate memory for record_info\n");
+                                       } 
+                }
+                           
+            call_info = (call_event_info_t*) malloc(sizeof(call_event_info_t)); 
             if (call_info) {
-               
                 call_info->domain = switch_safe_strdup(domain_name);
                 call_info->destination_number = switch_safe_strdup(callee_number);
                 call_info->event = switch_safe_strdup(event_name);
@@ -113,22 +137,14 @@ static void event_handler(switch_event_t *event) {
                 call_info->call_direction = switch_safe_strdup(call_direction);
 				call_info->call_uuid = switch_safe_strdup(call_uuid);
 				call_info->caller_name = switch_safe_strdup(caller_name);
+                call_info->recording_file_path = switch_safe_strdup("");
                 if(strcmp(event_name, "CHANNEL_PROGRESS_MEDIA")==0){
                     call_info->event = switch_safe_strdup("CS_RINGING");
                 }
                 else{
                     call_info->event =switch_safe_strdup(event_name);
                 }
-                if(strcmp(event_name, "RECORD_STOP")==0 && switch_file_exists(recording_file, NULL) == SWITCH_STATUS_SUCCESS){
-                    if(recording_file){
-                        call_info->recording_file_path = switch_safe_strdup(recording_file);
-                        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Recording file path: %s\n", recording_file);
-                    } 
-                }
-                else{
-                    call_info->recording_file_path = switch_safe_strdup("");
-                }
-				send_event_data(call_info);
+                send_event_data(call_info);
                 free_call_event_info(call_info);
             } else {
                 switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to allocate memory for call_info\n");
